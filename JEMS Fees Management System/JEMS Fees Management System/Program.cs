9e6d98a87a271ef23663474c1638201d4daccb59
barneystinson <shubham.sk473@gmail.com>
2015-11-18 02:19:12 +0530

    Regular Admission GUI minor change

--- JEMS Fees Management System/JEMS Fees Management System/Program.cs
+++ JEMS Fees Management System/JEMS Fees Management System/Program.cs
@@ -16,7 +16,7 @@ namespace JEMS_Fees_Management_System
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
