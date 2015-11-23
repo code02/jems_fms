e68fb1f582c43d3bff03272d072c3852855a2f6f
barneystinson <shubham.sk473@gmail.com>
2015-11-24 00:01:39 +0530

    Added Global Strings for db

--- JEMS Fees Management System/JEMS Fees Management System/GlobalVariables.cs
+++ JEMS Fees Management System/JEMS Fees Management System/GlobalVariables.cs
@@ -9,5 +9,23 @@ namespace JEMS_Fees_Management_System
     public static class GlobalVariables
     {
         public static String dbConnectString;
+
+
+
+        public static String session = "session";
+        public static String admission_rec_start = "admission_receipt_start";
+        public static String prov_rec_start = "provisional_receipt_start";
+        public static String ad_form_rec_start = "ad_form_receipt_start";
+        public static String annual_rec_start = "annual_receipt_start";
+        public static String monthly_rec_start = "monthly_receipt_start";
+        public static String other_rec_start = "other_receipt_start";
+        public static String belt_tie = "belt_tie";
+        public static String warn_fees_date = "warn_fees_date";
+        public static String default_warn_fees_date = "default_warn_fees_date";
+        public static String warn_fees = "warn_fees";
+        public static String late_fees = "late_fees";
+
+
+
     }
 }
